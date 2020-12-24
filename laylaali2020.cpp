00c448ac2327e6c6d72c6b760b9df738d1b28abe
lool2019 <47713368+lool2019@users.noreply.github.com>
2020-12-24 04:15:23 +0300

    Update laylaali2020.cpp

--- laylaali2020.cpp
+++ laylaali2020.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    cout << "I've participated in The Game's developers program!";
+int main()
+
+{
+
+    cout << "\n\n I've participated in The Game's developers program!\n\n" ;
     return 0;
 }
